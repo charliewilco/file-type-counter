5cf80da5428dd0e44294f75205cd7b6b4c872440
Charles ⚡ <charlespeters42@gmail.com>
2019-09-05 11:50:27 -0700

    first commit

---
+++ src/cli.ts
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+"use strict";
+
+import { extensionReporter } from ".";
+import * as mri from "mri";
+import * as chalk from "chalk";
+import { table,  getBorderCharacters } from "table";
+
+interface IArguments extends mri.Argv {
+  _: string[];
+  help: boolean;
+  h: boolean;
+}
+
+const args = <IArguments>mri(process.argv.slice(2), { boolean: ["h", "help"] });
+
+let help = `
+  Usage
+    $ extension-count <input>
+  Examples
+    $ extension-count ./src
+`;
+
+function getFileList(files: string[], limit: number | null = 10): string {
+    if (limit !== null && files.length > limit) {
+        return files.slice(0, limit).join("\n").concat(`\n${files.length - limit} more files`)
+    } else {
+        return files.join("\n")
+    }
+}
+
+function app() {
+  if (args.help || args.h) {
+    console.log(help);
+
+    return;
+  }
+
+  const data = extensionReporter(args._);
+
+  data.forEach(d => {
+    console.log("Results for: ", chalk.default.yellow(d.title), "\n");
+    const tableData = d.rows.map(row => {
+        
+
+        return [
+          chalk.default.blue(row.extension),
+          chalk.default.blue.bold(row.count.toString()),
+         getFileList(row.files, null)
+        ];
+      })
+    
+    const header = ["Extension", "File Count", "Files"];
+    const t: string = table(
+     [header, ...tableData], {
+         border:  getBorderCharacters("void"),
+         drawHorizontalLine: () => {
+            return true
+        }
+     }
+    );
+
+    console.log(t, "\n\n");
+  });
+}
+
+app();
---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import * as fs from "fs";
+
+interface IRow {
+  extension: string;
+  count: number;
+  files: string[];
+}
+
+interface StringTMap<T> {
+  [key: string]: T;
+}
+
+function fromEntries<U>(iterable: Map<string, U>): StringTMap<U> {
+  return [...iterable].reduce((obj: StringTMap<U>, [key, val]) => {
+    obj[key] = val;
+    return obj;
+  }, {});
+}
+
+function getFiles (dir: string, files_?: any[]){
+    files_ = files_ || [];
+    var files = fs.readdirSync(dir);
+    for (var i in files){
+        var name = dir + '/' + files[i];
+        if (fs.statSync(name).isDirectory()){
+            getFiles(name, files_);
+        } else {
+            files_.push(name);
+        }
+    }
+    return files_;
+}
+
+function createTable(folder: string): IRow[] {
+  const re: RegExp = /(?:\.([^.]+))?$/;
+  const map = new Map<string, Set<string>>();
+  const files = getFiles(folder)
+
+  files.forEach(file => {
+    const extension = re.exec(file)[0];
+
+    if (map.has(extension)) {
+      const set = map.get(extension);
+      set.add(file);
+    } else {
+      map.set(extension, new Set([file]));
+    }
+  });
+
+  const data = fromEntries(map);
+
+  return Object.keys(data).map<IRow>(key => {
+    return {
+      extension: key,
+      count: data[key].size,
+      files: Array.from(data[key])
+    };
+  });
+}
+
+interface ITableData {
+  title: string;
+  rows: IRow[];
+}
+
+export function extensionReporter(folders: string[]): ITableData[] {
+  return folders.map((folder: string) => {
+    return {
+      title: folder,
+      rows: createTable(folder)
+    };
+  });
+}
